2adda7c1ab93f8a101383c51ce38d4e2ebe64f54
sma11case <udf.q@qq.com>
2025-05-02 11:00:22 +0800

    add base64 logo support

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ ls -la
 date1=`date '+%Y-%m-%d %H:%M:%S'`
 git version && date1="$date1 -- $(git rev-parse --short HEAD)"
 
-LinkArgs="-buildid= -extldflags -static -w"
+LinkArgs="-buildid= -static -w -s -X"
 
 go mod tidy
-go build -a -trimpath -ldflags="$LinkArgs -s -X 'main.build=$date1'" .
+go build -a -trimpath -ldflags="$LinkArgs 'main.build=$date1'" .
 
 exit
