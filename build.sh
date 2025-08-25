70076db1928861dfdea1b62eebeeb673ac1d5685
sma11case <udf.q@qq.com>
2025-08-25 10:32:31 +0800

    v1

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ date1=`date '+%Y-%m-%d %H:%M:%S'`
 git version && date1="$date1 -- $(git rev-parse --short HEAD)"
 
 go mod tidy
+go get
 CGO_ENABLED=0 go build -a -trimpath -ldflags="-buildid= -w -s -X 'main.build=$date1'" .
 
 exit
